6a24672b41442c4b53ee132c47fd3545c31b76af
Heberth Alexandre <heberthax@gmail.com>
2020-05-20 12:36:31 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
-var div = document.getElementById("root");
+var div = document.getElementById("root").style.display = "none";
 var idade = document.querySelector("input[type=number]");
-console.log(idade);
+console.log(idade.value);
 var imagens = 
-["<img src='../img/teste1.jpg' alt='Imagem Teste'>", 
-"<img src='../img/teste2.jpg' alt='Imagem Teste'>"];
+["<img src='./img/teste1.jpg' alt='Imagem Teste'>", 
+"<img src='./img/teste2.jpg' alt='Imagem Teste'>"];
 
-function mudaIdade(){
+function mudaIdade(div){
     if(idade.value < 18){
-        div.style.display = "block";
+        div.style.display = "inline";
         div.innerHTML = imagens[0];
     }else{
-        div.style.display = "block";
+        div.style.display = "inline";
         div.innerHTML = imagens[1];
     }
 }
