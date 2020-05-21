2fb7b54ba067ec8f379e16be922637a805385a8a
Heberth Alexandre <heberthax@gmail.com>
2020-05-20 22:33:55 -0300

    Aplicação Concluída
    Finalização da Aplicação

--- js/index.js
+++ js/index.js
@@ -1,18 +1,46 @@
-var div = document.getElementById("root").style.display = "none";
-var idade = document.querySelector("input[type=number]");
-console.log(idade.value);
-var imagens = 
-["<img src='./img/teste1.jpg' alt='Imagem Teste'>", 
-"<img src='./img/teste2.jpg' alt='Imagem Teste'>"];
-
-function mudaIdade(div){
-    if(idade.value < 18){
-        div.style.display = "inline";
-        div.innerHTML = imagens[0];
+function resultado(){
+    let data = new Date();
+    let anoAtual = data.getFullYear();
+    let valorAno = document.getElementById("ano").value;
+    let genero;
+    let pResultado = document.getElementById("res");
+    let divFoto = document.getElementById("foto");
+    if(valorAno == 0 || valorAno > anoAtual){
+        alert("[Erro] - Digite um valor válido!");
     }else{
-        div.style.display = "inline";
-        div.innerHTML = imagens[1];
+        let sexo = document.getElementsByName("sexo");
+        let idade = anoAtual - valorAno;
+        let genero;
+        pResultado.style.textAlign = "center";
+        divFoto.style.textAlign = "center";
+        if(sexo[0].checked){
+            genero = "MASCULINO";
+            if(idade <= 3){
+                divFoto.innerHTML = "<img src='./img/masculino-bebe.jpg'>";
+            }else if((idade > 3) && (idade <= 12)){
+                divFoto.innerHTML = "<img src='./img/masculino-crianca.jpg'>";
+            }else if((idade > 12) && (idade <= 18)){
+                divFoto.innerHTML = "<img src='./img/masculino-adolescente.jpg'>";
+            }else if((idade > 18) && (idade <= 63)){
+                divFoto.innerHTML = "<img src='./img/masculino-adulto.jpg'>";
+            }else{
+                divFoto.innerHTML = "<img src='./img/masculino-idoso.jpg'>";
+            }
+        }else if(sexo[1].checked){
+            genero = "FEMININO";
+            if(idade <= 3){
+                divFoto.innerHTML = "<img src='./img/feminino-bebe.jpg'>";
+            }else if((idade > 3) && (idade <= 12)){
+                divFoto.innerHTML = "<img src='./img/feminino-crianca.jpg'>";
+            }else if((idade > 12) && (idade <= 18)){
+                divFoto.innerHTML = "<img src='./img/feminino-adolescente.jpg'>";
+            }else if((idade > 18) && (idade <= 63)){
+                divFoto.innerHTML = "<img src='./img/feminino-adulto.jpg'>";
+            }else{
+                divFoto.innerHTML = "<img src='./img/feminino-idoso.jpg'>";
+            }
+        }
+        divFoto.style.display = "block";
+        pResultado.innerText = `Verificamos gênero ${genero} com ${idade} anos`;
     }
-}
-
-mudaIdade();
+}   
